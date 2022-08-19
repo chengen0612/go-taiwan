51968d68b37cdeb2e84b5ba89c92bda80984632e
evans <chengen0612@gmail.com>
2022-08-19 10:07:59 +0800

    chore: application setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Fake from "#/components/Fake"
+
+function App() {
+  return (
+    <div>
+      Hi
+      <Fake />
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Fake.tsx
@@ -0,0 +1,5 @@
+function Fake () {
+    return <div>This is fake child</div>;
+}
+
+export default Fake
