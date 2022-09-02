2630350edc3f3eeda4fbf1a2e20e92176b421af9
evans <chengen0612@gmail.com>
2022-09-02 17:56:11 +0800

    feat: add slide bar to pick up city

---
+++ src/components/search/SlideCity.tsx
@@ -0,0 +1,81 @@
+import { memo, useCallback, MouseEventHandler } from "react";
+import { styled } from "@mui/material/styles";
+import Container, { ContainerProps } from "@mui/material/Container";
+import ButtonBase, { ButtonBaseProps } from "@mui/material/ButtonBase";
+
+import { useAppSelector, useAppDispatch } from "#/utils/hooks/store";
+import {
+  selectSearchCity,
+  setSearch,
+  SetSearchPayload,
+} from "#/store/slices/search";
+
+import { CITY } from "#/utils/constants/city";
+import { SearchProperty } from "#/utils/types/search";
+
+const StyledContainer = styled(Container)<ContainerProps>({
+  height: "4.5rem",
+  display: "flex",
+  alignItems: "center",
+  columnGap: "0.5rem",
+  overflowX: "scroll",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+});
+
+interface CityButtonProps extends ButtonBaseProps {
+  selected: boolean;
+}
+
+const CityButton = memo(
+  styled(ButtonBase, {
+    shouldForwardProp: (prop) => prop !== "selected",
+  })<CityButtonProps>(({ selected, theme }) => ({
+    height: "3rem",
+    aspectRatio: "1/1",
+    borderRadius: "50%",
+    fontColor: theme.palette.grey[800],
+    ...(selected && {
+      backgroundColor: theme.palette.grey[200],
+    }),
+  }))
+);
+
+function SlideCity() {
+  const selectedCity = useAppSelector(selectSearchCity);
+  const appDispatch = useAppDispatch();
+
+  const handleClick = useCallback<MouseEventHandler<HTMLElement>>(
+    (event) => {
+      if (event.target instanceof HTMLElement) {
+        const cityKey = event.target.dataset!.key;
+
+        const payload = {
+          searchProperty: SearchProperty.City,
+          value: cityKey,
+        } as SetSearchPayload;
+
+        appDispatch(setSearch(payload));
+      }
+    },
+    [appDispatch]
+  );
+
+  return (
+    <StyledContainer>
+      {CITY.all.map((item) => (
+        <CityButton
+          key={item.key}
+          data-key={item.key}
+          selected={item.key === selectedCity}
+          onClick={handleClick}
+        >
+          {item.value}
+        </CityButton>
+      ))}
+    </StyledContainer>
+  );
+}
+
+export default SlideCity;
