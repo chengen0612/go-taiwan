30702d41b572f647d7ee377489a108f53a470b21
evans <chengen0612@gmail.com>
2022-09-06 11:44:12 +0800

    chore: adjust configuration and types declaration of vite to implement svgr

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
+/* Support type inference when loading svg as ReactComponent. */
+/// <reference types="vite-plugin-svgr/client" />
+
 /// <reference types="vite/client" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import path from "node:path";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: [{ find: "#", replacement: path.resolve(__dirname, "src") }],
   },
