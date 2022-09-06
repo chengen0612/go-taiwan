7770c0f57e52c15959080802815c9254a853ac6f
evans <chengen0612@gmail.com>
2022-09-06 17:29:38 +0800

    feat: adjust background color

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -17,6 +17,7 @@ function PageLayout() {
             "0px 0px 2px rgba(40, 41, 61, 0.04), 0px 4px 8px rgba(96, 97, 112, 0.16)",
           pt: "1rem",
           px: 0,
+          bgcolor: "common.white",
         }}
       >
         <SearchBar />
