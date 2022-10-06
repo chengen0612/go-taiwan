5c671599a1eef061e9008d36af7b4c8f7246c64b
evans <chengen0612@gmail.com>
2022-10-06 16:44:24 +0800

    feat: add error type

--- src/utils/helpers/http-error.ts
+++ src/utils/helpers/http-error.ts
@@ -1,4 +1,4 @@
-const recordedErrorCode = [403, 404, 500, 503] as const;
+const recordedErrorCode = [403, 404, 429, 500, 503] as const;
 
 type RecordedErrorCode = typeof recordedErrorCode[number];
 type ErrorCode = RecordedErrorCode | number;
@@ -7,13 +7,14 @@ const isRecordedError = (code: ErrorCode): code is RecordedErrorCode =>
   recordedErrorCode.includes(code as RecordedErrorCode);
 
 const DEFAULT_ERROR_MESSAGE =
-  "Oops，似乎發生了一點問題，請嘗試其他搜尋或稍後再來";
+  "Oops，似乎發生了一點問題，請嘗試其他搜尋或稍後再回來";
 const SERVER_ERROR_MESSAGE = "目前無法取得資料，請稍後再試";
 
 const getErrorMessage = (code: RecordedErrorCode) =>
   ({
-    403: "很抱歉，您的單日搜尋次數已達上限，請下次再來",
+    403: "很抱歉，您沒有權限訪問此資料",
     404: "您所訪問的頁面不存在，請嘗試其他搜尋",
+    429: "很抱歉，您的單日搜尋次數已達上限，請下次再來",
     500: SERVER_ERROR_MESSAGE,
     503: SERVER_ERROR_MESSAGE,
   }[code]);
