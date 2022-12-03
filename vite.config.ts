3f594d07cc8a20c49e4be5921e38264f3f383c64
evans <chengen0612@gmail.com>
2022-12-03 20:08:03 +0800

    chore: remove the base option in vite config to run the app on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/go-taiwan/",
+  // The base option is for GitHub Page to serve the app from the subfolder.
+  // Comment out since the app is now running on Vercel.
+  // base: "/go-taiwan/",
   plugins: [svgr(), react()],
   resolve: {
     alias: [{ find: "#", replacement: path.resolve(__dirname, "src") }],
